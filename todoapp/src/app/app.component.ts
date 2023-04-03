fd541bdb4b59d7829cecf1d217a84e27c18efe4c
Tran Thanh Phong <tranthanhphong91@gmail.com>
2023-04-03 20:45:21 +0700

    Topic 010 - Template Variable & ViewChild/ViewChildren

--- todoapp/src/app/app.component.ts
+++ todoapp/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, QueryList, ViewChild, ViewChildren } from '@angular/core';
+import { ToggleComponent } from './toggle/toggle.component';
 
 @Component({
   selector: 'app-root',
@@ -8,4 +9,28 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'todoapp';
   checked: boolean = false;
+  @ViewChild('toggleComp2') toggleComp!: ToggleComponent;
+  @ViewChild('textContainer') textContainer!: ElementRef<HTMLDivElement>;
+  @ViewChildren(ToggleComponent) toggles!: QueryList<ToggleComponent>;
+
+  ngAfterViewInit() {
+    console.log(this.toggles);
+  }
+
+  doSomething() {
+    this.checked = !this.checked;
+    console.log(this.checked);
+  }
+
+  toggleInside() {
+    this.toggleComp.toggle();
+  }
+
+  setDivText() {
+    if (this.textContainer.nativeElement.textContent === 'New') {
+      this.textContainer.nativeElement.textContent = 'Old';
+    } else {
+      this.textContainer.nativeElement.textContent = 'New';
+    }
+  }
 }
